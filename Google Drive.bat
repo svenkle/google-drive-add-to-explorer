44b0d25ba2bf4414618f9aa48f142c6df8d7140f
Rohit Ghosh <jendermine@gmail.com>
2021-10-17 17:05:12 +0530

    added checking for admin rights

--- Google Drive.bat
+++ Google Drive.bat
@@ -1,5 +1,13 @@
 @ECHO OFF
 
+::Checking for administrator rights using fsutil which will only work iff under admin rights
+::which just queries the volume here %systemdrive% for a 'dirty bit' for corrupted volume 
+::WITHOUT TOUCHING ANY FILES/REGISTERY ANYTHING at all
+::source >> https://stackoverflow.com/a/21295806
+
+fsutil dirty query %systemdrive% >nul
+IF %ERRORLEVEL% GTR 0 goto NO_ADMIN_ERROR
+
 REG ADD "HKCU\Software\Classes\Wow6432Node\CLSID\{81539FE6-33C7-4CE7-90C7-1C7B8F2F2D41}" /ve /t REG_SZ /d "Google Drive" /f
 REG ADD "HKCU\Software\Classes\Wow6432Node\CLSID\{81539FE6-33C7-4CE7-90C7-1C7B8F2F2D41}" /v "System.IsPinnedToNamespaceTree" /t REG_DWORD /d "1" /f
 REG ADD "HKCU\Software\Classes\Wow6432Node\CLSID\{81539FE6-33C7-4CE7-90C7-1C7B8F2F2D41}" /v "SortOrderIndex" /t REG_DWORD /d "66" /f
@@ -28,3 +36,14 @@ IF EXIST "%USERPROFILE%\\My Drive" (
 	REG ADD "HKCU\Software\Classes\CLSID\{81539FE6-33C7-4CE7-90C7-1C7B8F2F2D41}\Instance\InitPropertyBag" /v "TargetFolderPath" /t REG_SZ /d "G:\\" /f
 	REG ADD "HKCU\Software\Classes\Wow6432Node\CLSID\{81539FE6-33C7-4CE7-90C7-1C7B8F2F2D41}\Instance\InitPropertyBag" /v "TargetFolderPath" /t REG_SZ /d "G:\\" /f
 )
+
+:NO_ADMIN_ERROR
+
+::The error msg is here just for reference, you are free and encouraged to add your own
+
+echo =====================================================
+echo This script needs to be executed as an administrator.
+echo =====================================================
+echo .
+pause
+goto :EOF
